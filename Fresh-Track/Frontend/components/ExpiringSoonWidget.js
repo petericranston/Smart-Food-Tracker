70bf6c9ce13732ebb049404468bcaeb2df5e7da7
BForoughi <147166034+BForoughi@users.noreply.github.com>
2026-04-03 11:57:23 +0100

    fixing logic and styling issues - need to add dynamic styling

--- Fresh-Track/Frontend/components/ExpiringSoonWidget.js
+++ Fresh-Track/Frontend/components/ExpiringSoonWidget.js
@@ -4,17 +4,18 @@ import { RFValue } from "react-native-responsive-fontsize";
 export default function ExpiringWidget({ image, name, expireMessage, expiringIn, dateStyling }){
     return(
         <View style={{ 
-            flexDirection: "row", gap: 10, marginBottom: 20, 
-            borderColor: "#B5B5B540", borderWidth: 2, padding: 20, 
-            borderRadius: 10, backgroundColor: "#fff" 
+            flexDirection: "row", marginBottom: 20, 
+            borderColor: "#B5B5B540", borderWidth: 2, paddingBottom: 20, paddingTop: 20, 
+            paddingLeft: 10, paddingRight: 10,
+            borderRadius: 10, backgroundColor: "#fff", alignItems: "center", justifyContent: "space-evenly" 
         }}>
-            <Image source={image} style={{ width: 50, height: 50 }}></Image>
+            <Image source={image} style={{ width: 60, height: 60 }}></Image>
             <View style={{ flexDirection: "column" }}>
                 <Text style={{fontSize: RFValue(16), marginBottom: 5, fontFamily: 'Inter_500Medium'}}>{name}</Text>
                 <Text style={{ fontSize: RFValue(9), width: "70%", color: "#8E8E8E"}}>{expireMessage}</Text>
             </View>
             <View style={dateStyling}>
-                <Text>{expiringIn}</Text>
+                <Text style={{ color: "white" }}>{expiringIn}</Text>
             </View>
         </View>
     )
--- Fresh-Track/Frontend/pages/Home.js
+++ Fresh-Track/Frontend/pages/Home.js
@@ -1,5 +1,5 @@
 import { SafeAreaView } from "react-native-safe-area-context"
-import { Text, StyleSheet, View, TextInput } from "react-native"
+import { Text, StyleSheet, View, TextInput, ScrollView } from "react-native"
 import { RFValue } from 'react-native-responsive-fontsize';
 import DashboardWidget from "../components/dashboardWidget";
 import ExpiringWidget from "../components/ExpiringSoonWidget";
@@ -7,77 +7,78 @@ import { Feather } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
 
 export default function Home(){
-
     const dummyItems = ["chicken", "beef", "yogurt", "milk", "eggs", "broccoli", "apples", "mango", "cake", "cream", "protein shake", "mayo", "bread"];
     const dummyExpiring = [
-        {id: 1, name: "chicken", expiryDate: "2026-03-27"},
-        {id: 2, name: "milk", expiryDate: "2026-03-30"},
-        {id: 3, name: "eggs", expiryDate: "2026-04-03"}
+        {id: 1, name: "chicken", expiryDate: "2026-04-10"},
+        {id: 2, name: "milk", expiryDate: "2026-04-06"},
+        {id: 3, name: "eggs", expiryDate: "2026-04-20"}
     ];
 
     const navigation = useNavigation()
 
     return(
         <>
-            <SafeAreaView style={styles.container}>
-                <Text style={styles.h2} >Good day</Text>
-                <Text style={styles.h1} >Your Kitchen</Text>
+            <ScrollView showsVerticalScrollIndicator={false} style={{ backgroundColor: '#F8F5EC'}}>
+                <SafeAreaView style={styles.container}>
+                    <Text style={styles.h2} >Good day</Text>
+                    <Text style={styles.h1} >Your Kitchen</Text>
 
-                <View style={styles.mainContent}>
-                    {/* search bar */}
-                    <View style={styles.searchBar}>
-                        <Feather name="search" size={18} color="#697665" />
-                        <TextInput
-                            style={styles.searchInput}
-                            placeholder="Search items..."
-                            placeholderTextColor="#697665"
-                        />
-                    </View>
+                    <View style={styles.mainContent}>
+                        {/* search bar */}
+                        <View style={styles.searchBar}>
+                            <Feather name="search" size={18} color="#697665" />
+                            <TextInput
+                                style={styles.searchInput}
+                                placeholder="Search items..."
+                                placeholderTextColor="#697665"
+                            />
+                        </View>
 
-                    <View style={ styles.dashWidgetContainer}>
-                        <DashboardWidget count={dummyItems.length} title="Items Tracked"/>
-                        <DashboardWidget count={dummyExpiring.length} title="Expiring Soon"/>
-                    </View>
+                        <View style={ styles.dashWidgetContainer}>
+                            <DashboardWidget count={dummyItems.length} title="Items Tracked"/>
+                            <DashboardWidget count={dummyExpiring.length} title="Expiring Soon"/>
+                        </View>
 
-                    <View style={{ marginTop: 50 }}>
-                        {dummyExpiring.length > 0 ? (
-                            <>
-                                <View style={{ flexDirection: "row", alignItems: "center", justifyContent: "space-between", paddingRight: 2, marginBottom: 50}}>
-                                    <Text style={styles.h3}>Needs Attention</Text>
-                                    <Text style={{ color: "#50863F", textDecorationLine: "underline"}} onPress={() => dummyExpiring}>See All</Text>
-                                </View>
+                        <View style={{ marginTop: 50 }}>
+                            {dummyExpiring.length > 0 ? (
+                                <>
+                                    <View style={{ flexDirection: "row", alignItems: "center", justifyContent: "space-between", paddingRight: 2, marginBottom: 50}}>
+                                        <Text style={styles.h3}>Needs Attention</Text>
+                                        <Text style={{ color: "#50863F", textDecorationLine: "underline"}} onPress={() => dummyExpiring}>See All</Text>
+                                    </View>
+                                    <View>
+                                        {
+                                            dummyExpiring.map((item) => {
+                                                const today = new Date();
+                                                const expiry = new Date(item.expiryDate);
+
+                                                const diffTime = expiry - today;
+                                                const daysLeft = Math.ceil(diffTime / (1000 * 60 * 60 * 24)); // convert ms to days
+                                                return <ExpiringWidget 
+                                                    image={require("../assets/foodplaceholders/mschicken.png")} name={item.name} 
+                                                    expireMessage={`This item is expiring in ${daysLeft} days!`} expiringIn={`${daysLeft} days`} 
+                                                    dateStyling={{backgroundColor: "red", justifyContent: "center", paddingLeft: 10, paddingRight: 10, height: 33, marginTop: 5, borderRadius: 10}}
+                                                />
+                                            })
+                                        }
+                                    </View>
+                                </>
+                                
+                            ) : dummyItems.length > 0 && dummyExpiring.length <= 0 ? (
+                                <View><Text>dummy</Text></View>
+                            ) : (
                                 <View>
-                                    {
-                                        dummyExpiring.map((item) => {
-                                            const today = new Date();
-                                            const expiry = new Date(dummyExpiring.expiryDate);
-
-                                            const diffTime = expiry - today;
-                                            const daysLeft = Math.ceil(diffTime / (1000 * 60 * 60 * 24)); // convert ms to days
-                                            return <ExpiringWidget 
-                                                image={require("../assets/foodplaceholders/mschicken.png")} name={item.name} 
-                                                expireMessage={`This item is expiring in ${daysLeft} days!`} expiringIn={daysLeft} 
-                                                dateStyling={{backgroundColor: "red", justifyContent: "center"}}
-                                            />
-                                        })
-                                    }
+                                    <Text 
+                                        style={{ textDecorationLine: "underline", fontSize: RFValue(20), fontFamily: 'Inter_600SemiBold'}}
+                                        onPress={() => navigation.navigate("AddItemsPage")}>Add items to get started
+                                    </Text>
                                 </View>
-                            </>
-                            
-                        ) : dummyItems.length > 0 && dummyExpiring.length <= 0 ? (
-                            <View><Text>dummy</Text></View>
-                        ) : (
-                            <View>
-                                <Text 
-                                    style={{ textDecorationLine: "underline", fontSize: RFValue(20), fontFamily: 'Inter_600SemiBold'}}
-                                    onPress={() => navigation.navigate("AddItemsPage")}>Add items to get started
-                                </Text>
-                            </View>
-                        )}
-                    </View>
+                            )}
+                        </View>
 
-                </View>
-            </SafeAreaView>
+                    </View>
+                </SafeAreaView>
+            </ScrollView>
         </>
     )
 }
