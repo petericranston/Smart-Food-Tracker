f06fb27a80b18c90e139394414dffbfd3ced2058
BForoughi <147166034+BForoughi@users.noreply.github.com>
2026-04-15 12:45:56 +0100

    fixing merge

--- Fresh-Track/Frontend/pages/AddItems.js
+++ Fresh-Track/Frontend/pages/AddItems.js
@@ -109,80 +109,73 @@ export default function AddItems() {
 
   return (
     <>
-      <SafeAreaView style={styles.container}>
-        <Text style={styles.h1}>Add items</Text>
-        <Text style={styles.h2}>Choose how to add your food</Text>
+        <SafeAreaView style={styles.container}>
+            <Text style={styles.h1}>Add items</Text>
+            <Text style={styles.h2}>Choose how to add your food</Text>
 
-        <View style={{ alignItems: "center", marginTop: 15 }}>
-          <View style={{ flexDirection: "row", gap: 20, marginBottom: 23 }}>
+            <View style={{ alignItems: "center", marginTop: 15 }}>
+            <View style={{ flexDirection: "row", gap: 20, marginBottom: 23 }}>
+                <ItemInput
+                inputChoice={"Scan receipt"}
+                onPressFunction={() => setReceiptVisible(true)}
+                >
+                <Ionicons name="receipt-outline" color="white" size={40} />
+                </ItemInput>
+                <ItemInput
+                inputChoice={"Scan barcode"}
+                onPressFunction={() => setBarcodeVisible(true)}
+                >
+                <Ionicons name="barcode-outline" size={40} color="white" />
+                </ItemInput>
+            </View>
+
             <ItemInput
-              inputChoice={"Scan receipt"}
-              onPressFunction={() => setReceiptVisible(true)}
+                inputChoice={"Search item"}
+                onPressFunction={() => setSearchVisible(true)}
             >
-              <Ionicons name="receipt-outline" color="white" size={40} />
+                <Feather name="search" size={40} color="white" />
             </ItemInput>
-            <ItemInput
-              inputChoice={"Scan barcode"}
-              onPressFunction={() => setBarcodeVisible(true)}
-            >
-              <Ionicons name="barcode-outline" size={40} color="white" />
-            </ItemInput>
-          </View>
+            </View>
 
-          <ItemInput
-            inputChoice={"Search item"}
-            onPressFunction={() => setSearchVisible(true)}
-          >
-            <Feather name="search" size={40} color="white" />
-          </ItemInput>
-        </View>
+            {/* Divider line */}
+            <View
+            style={{
+                height: 1,
+                backgroundColor: "#B5B5B549",
+                marginTop: 25,
+                shadowColor: "#000000",
+                shadowOffset: { width: 2, height: 2 },
+                shadowOpacity: 0.8,
+                shadowRadius: 4,
+                elevation: 4,
+            }}
+            />
 
-        {/* Divider line */}
-        <View
-          style={{
-            height: 1,
-            backgroundColor: "#B5B5B549",
-            marginTop: 25,
-            shadowColor: "#000000",
-            shadowOffset: { width: 2, height: 2 },
-            shadowOpacity: 0.8,
-            shadowRadius: 4,
-            elevation: 4,
-          }}
-        />
-
-        <View
-          style={{
-            marginTop: 25,
-            padding: 15,
-            borderColor: "#B5B5B550",
-            borderWidth: 2,
-          }}
-        >
-          {scannedItems.length > 0 ? (
-            scannedItems.map((item) => {
-              const displayUnit = item.unit
-                ? `${item.quantity} x ${item.unit}`
-                : `${item.quantity} x`;
-
-              return (
-                <View style={styles.itemsRow} key={item.id}>
-                  <Text style={{ fontSize: RFValue(14) }}>
-                    {displayUnit} {item.name}
-                  </Text>
-                  <Text style={{ color: "#888", fontSize: RFValue(14) }}>
-                    {formatExpiry(item.expiryDate)}
-                  </Text>
-                </View>
-              );
-            })
-          ) : (
-            <Text style={{ textAlign: "center", fontSize: RFValue(14) }}>
-              Added items will show here!
-            </Text>
-          )}
-        </View>
-      </SafeAreaView>
+            <View style={{ marginTop: 25, padding: 15, borderColor: "#B5B5B550", borderWidth: 2, maxHeight: 300, minHeight: 50}}>
+                <FlatList
+                    data={scannedItems}
+                    keyExtractor={(item) => item.id.toString()}
+                    ListEmptyComponent={
+                    <Text style={{ textAlign: "center", fontSize: RFValue(14) }}>
+                        Added items will show here!
+                    </Text>
+                    }
+                    renderItem={({ item }) => {
+                        const displayUnit = item.unit ? `${item.quantity} x ${item.unit}` : `${item.quantity} x`;
+                        return (
+                            <View style={styles.itemsRow}>
+                            <Text numberOfLines={3} style={{ fontSize: RFValue(14), width: "70%" }}>
+                                {displayUnit} {item.name}
+                            </Text>
+                            <Text style={{ color: "#888", fontSize: RFValue(14) }}>
+                                {formatExpiry(item.expiryDate)}
+                            </Text>
+                            </View>
+                        );
+                    }}
+                />
+            </View>
+        </SafeAreaView>
 
       {/* ---------Modals--------- */}
 
